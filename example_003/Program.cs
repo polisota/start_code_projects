1cda1e96b332b1767c67c5acab497ade5a847edf
Polina K <polisota@mail.ru>
2023-02-07 19:07:19 +0300

    add random

--- example_003/Program.cs
+++ example_003/Program.cs
@@ -1,4 +1,10 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿//int numberA = 3;
+//int numberB = 5;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(result);
