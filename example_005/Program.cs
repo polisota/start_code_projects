d89e273c588726e250e8e899d7324c333b8806c4
Polina K <polisota@mail.ru>
2023-02-07 19:19:11 +0300

    example_005 folder added

---
+++ example_005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Ваше имя: ");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "маша")
+{
+    Console.Write("Здравствуй, " + userName);
+}
+else
+{
+    Console.Write("Привет, " + userName);
+}
