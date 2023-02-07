83a8e651e7d4041d902bc54a3399168e8a988666
Polina K <polisota@mail.ru>
2023-02-07 19:25:51 +0300

    example_006 folder added

---
+++ example_006/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 5;
+int b = 3;
+int c = 8;
+int d = 1;
+int e = 4;
+
+int max = a;
+
+if (a > max) max =	a;
+if (b > max) max =	b;
+if (c > max) max =	c;
+if (d > max) max =	d;
+if (e > max) max =	e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
