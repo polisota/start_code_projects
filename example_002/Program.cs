7f97df6a35d941d94d04f0f8f4408c02473a4810
Polina K <polisota@mail.ru>
2023-02-07 15:26:44 +0300

    example_002 folder added

---
+++ example_002/Program.cs
@@ -0,0 +1,7 @@
+﻿//Console.WriteLine("Введите ваше имя: ");
+Console.Write("Введите ваше имя: ");
+string userName = Console.ReadLine();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(userName);
+//Console.WriteLine("Привет, " + userName);
+Console.Write("Привет, " + userName);
