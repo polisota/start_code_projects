b4e3c8c95483bb06fa41b224a06fe06bebd7fc5e
Polina K <polisota@mail.ru>
2023-02-21 21:11:07 +0300

    second and third lections folders added

---
+++ Lessons/L3/ex3_001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+string Method(int count, string text)
+{
+    string result = String.Empty;
+    
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+
+    return result;
+}
+
+string res = Method(10, "z");
+Console.WriteLine(res);
---
+++ Lessons/L3/ex3_002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Таблица умножения");
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
