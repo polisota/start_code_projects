9b76135eadff32b745393f73404a560487cc456d
Polina K <polisota@mail.ru>
2023-02-24 21:48:06 +0300

    fourth seminar folder added

--- Seminars/ex_01/Program.cs
+++ Seminars/ex_01/Program.cs
@@ -1,11 +1,27 @@
 ﻿Console.Clear();
-Console.Write("Введите 1-ое число: ");
+// Console.Write("Введите 1-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (n > m)
+//     Console.WriteLine(n);
+// else if (n < m)
+//     Console.WriteLine(m);
+// else
+//     Console.WriteLine("=");
+
+// Array
+
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2-ое число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-if (n > m)
-    Console.WriteLine(n);
-else if (n < m)
-    Console.WriteLine(m);
-else
-    Console.WriteLine("=");
+double[] array = new double[n];
+int begin = 10, end = 20;
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+
+foreach (double element in array)
+{
+    Console.WriteLine(element);
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
---
+++ Seminars/tasks_sem_4/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+
+//Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+Console.Write("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат: {(1 + n) / 2 * n}");
+
+//Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int i=0;
+while(a>0) 
+{
+    i++;
+    a/=10;
+}
+Console.WriteLine($"Результат: {i}");
+
+//Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+Console.Write("Введите число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for (int j = 1; j<=b; j++)
+{
+    res *= j;
+
+}
+Console.WriteLine($"Результат: {res}");
+
+//Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+int[] array = new int[8];
+for (int m = 0; m < array.Length; m++)
+    array[m] = new Random().Next(0, 2); 
+Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
